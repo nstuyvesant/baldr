0cae214bc978a9a0130bf337f83abab26ec921be
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-06-12 19:38:28 -0400

    chore(html): dynamically populate data from JSON

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const express = require('express')
 const Client = require('pg-native')
 const app = express()
+const path = require('path')
+const pgConnectionString = 'postgresql://postgres@localhost:5432/vr'
 
-app.get('/', (req, res) => {
+// Generic HTTP GET uses /public for static content
+app.get('/', express.static(path.join(__dirname, 'public')))
+
+// Pass logo explicitly with correct MIME type
+app.get('/perfecto-logo.svg', (req, res, next) => {
+  res.setHeader('Content-Type', 'image/svg+xml');
+  res.sendFile(path.join(__dirname, 'public/perfecto-logo.svg'));
+});
+
+// Simple API - test using http://localhost:3000/cloud/acme.perfectomobile.com?date=2018-06-12
+app.get('/cloud/:id', (req, res, next) => {
   let client = new Client()
-  client.connect('postgresql://postgres@localhost:5432/vr', (err) => {
+  let cloudId = req.params.id
+  let snapshotDate = req.query.date
+  client.connect(pgConnectionString, (err) => {
     if (err) throw err
-    client.query("SELECT cloudSnapshot('acme.perfectomobile.com', '2018-06-12'::DATE)", (err, rows)=> {
-      res.send(rows[0].cloudsnapshots)
+    // TODO: Later, change this to pass uuid for cloud instead of fqdn (for security reasons)
+    // TODO: As a best practice, we should prevent SQL injection
+    let sql = `SELECT cloudSnapshot('${cloudId}', '${snapshotDate}'::DATE)`
+    client.query(sql, (err, rows)=> {
+      if (err) throw err
+      res.send(rows[0].cloudsnapshot)
     })
   })
-  
-  // TODO: Get fqdn (fully-qualified domain name) and date from a querystring and pass as variables to the line above
 })
 
-app.listen(3000, () => console.log('Report server listening on port 3000.'))
+app.listen(3000, () => console.log('Baldr listening on port 3000.'))
