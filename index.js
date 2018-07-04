548756889d6e063896033d6574dfe6dcba0187c5
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-07-04 08:25:52 -0400

    debug(post): uploads

--- index.js
+++ index.js
@@ -117,6 +117,7 @@ app.post('/api', authenticate, (req, res) => {
       return
     }
     let sql = `SELECT json_snapshot_upsert($1::json)` // parameterized to prevent SQL injection
+    console.log('jsonSnapshot', jsonSnapshot)
     client.query(sql, [jsonSnapshot], (err)=> {
       if (err) {
         console.log(err)
