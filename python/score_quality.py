2db6587bf3fbe4a5f7af39beee44685337c88b73
Ran Bechor <ranb@MacBook-Pro-2.local>
2018-08-06 14:40:58 -0400

    score configuration support

--- python/score_quality.py
+++ python/score_quality.py
@@ -2,6 +2,8 @@ from simple_salesforce import Salesforce
 import psycopg2
 import logging
 from logging.handlers import RotatingFileHandler
+import configparser
+
 
 ## config logging
 def config_logging(log_name, log_path):
@@ -34,32 +36,35 @@ def exec_query(query ,mul ,days):
     total = 0
     res = sf.query_all(query)
     for record in res['records']:
+
+        global_logger.info(str(mul) +" - " + str(record['Age_Custom__c']) +" * " + str(days))
         total += mul - record['Age_Custom__c'] * days
+        global_logger.info(total)
 
     return round(total)
 
 ## run the queries on the salesfore db and return result
 def run_queries(cloud):
-    try:
 
+    try:
         defect = "SELECT Age_Custom__c FROM Case WHERE Case.Type = 'Defect' AND Case.CreatedDate >= LAST_N_DAYS:30 AND Case.AppURL__c ='" + cloud + "' "
-        defect = exec_query(defect, 15, 0.5)
+        defect = exec_query(defect, 15, defect_weight/defect_days)
         global_logger.info("defect " + str(defect))
 
         service_degradation = "SELECT Age_Custom__c FROM Case WHERE Case.Case_Reason__c = 'Cloud: Service Degradation' AND Case.CreatedDate >= LAST_N_DAYS:30 AND Case.AppURL__c ='" + cloud + "' "
-        service_degradation = exec_query(service_degradation, 15, 0.5)
+        service_degradation = exec_query(service_degradation, 15, service_degradation_weight/service_degradation_days)
         global_logger.info("service_degradation " + str(service_degradation))
 
         outage = "SELECT Age_Custom__c FROM Case WHERE Case.Case_Reason__c = 'Cloud: Outage' AND Case.CreatedDate >= LAST_N_DAYS:90 AND Case.AppURL__c ='" + cloud + "' "
-        outage = exec_query(outage, 25, 0.16667)
+        outage = exec_query(outage, 25, (outage_weight/outage_days))
         global_logger.info("outage " + str(outage))
 
         maintenance = "SELECT Age_Custom__c FROM Case WHERE Case.Case_Reason__c = 'Cloud: Maintenance issue' AND Case.CreatedDate >= LAST_N_DAYS:90 AND Case.AppURL__c ='" + cloud + "'"
-        maintenance = exec_query(maintenance, 50, 0.16667)
+        maintenance = exec_query(maintenance, 50, (maintenance_weight/maintenance_days))
         global_logger.info("maintenance " + str(maintenance))
 
         hot_list = "SELECT Age_Custom__c FROM Case WHERE Case.Hot_List__c = 'Hot-R&D' AND Case.CreatedDate >= LAST_N_DAYS:60 AND Case.AppURL__c ='" + cloud + "'"
-        hot_list = exec_query(hot_list, 25, 0.25)
+        hot_list = exec_query(hot_list, 25, (hot_list_weight/hot_list_days))
         global_logger.info("hot_list " + str(hot_list))
 
         score = int (100 - round(defect + service_degradation + outage + maintenance + hot_list))
@@ -69,8 +74,21 @@ def run_queries(cloud):
         return None
 
 ##MAIN
-
 global_logger = config_logging("salesforce", "/var/log/score_quality.log")
+config = configparser.ConfigParser()
+config.read('score.conf')
+defect_weight=  float(config['WEIGHT']['defect'])
+service_degradation_weight =  float(config['WEIGHT']['service_degradation'])
+outage_weight = float(config['WEIGHT']['outage'])
+maintenance_weight= float(config['WEIGHT']['maintenance'])
+hot_list_weight= float(config['WEIGHT']['hot_list'])
+
+defect_days = float(config['DAYS']['defect'])
+service_degradation_days = float(config['DAYS']['service_degradation'])
+outage_days = float(config['DAYS']['outage'])
+maintenance_days = float(config['DAYS']['maintenance'])
+hot_list_days = float(config['DAYS']['hot_list'])
+
 
 try:
     # loop on the clouds table
@@ -82,15 +100,16 @@ try:
         if row == None:
             break
         # get the cloud score
+        global_logger.info("***********______ " + row[0] + "______************")
         score =run_queries(row[0])
         # update the score in the snapshot table if score is not null
         if score : update_records(score,row[1])
-        global_logger.info( "cloud " + row[0] +" id " + row[1] + " + score : " + str(score))
+        global_logger.info(" id " + row[1] + " + score : " + str(score))
+
 
 except psycopg2.DatabaseError, e:
     if con:
         con.rollback()
-
 
 
 
