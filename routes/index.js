f4985a481ef705d9248c2d05c12a350cb700a2f5
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-12-09 11:04:45 -0500

    refactor(routes): simplify

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,8 @@
 /*
-  Central file for setting up all routes (we only have one for now)
+  Central place for setting up  routes (we only have one for now)
 */
-
-// Load API routes
-const api = require('./api')
 
 // This is how we connect the route handling to the central instance of ExpressJS
 module.exports = (app) => {
-  app.use('/api', api)
+  app.use('/api', require('./api'))
 }
